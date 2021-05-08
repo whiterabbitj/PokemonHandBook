6c9cf80b020d7ca6107a51c6930072c3db04ef86
Tomas Sukevicius <tomas.sukevicius@gabarnie.co.uk>
2021-05-08 03:09:08 +0100

    Wifi check

--- PokemonHanbook/app/src/main/java/com/example/pokemonhanbook/Activities/MainActivity.kt
+++ PokemonHanbook/app/src/main/java/com/example/pokemonhanbook/Activities/MainActivity.kt
@@ -1,19 +1,41 @@
 package com.example.pokemonhanbook.Activities
 
+import android.app.AlertDialog
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.content.DialogInterface
+import android.net.ConnectivityManager
 import android.os.Bundle
-import android.widget.ProgressBar
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pokemonhanbook.Adpaters.ItemAdapter
 import com.example.pokemonhanbook.R
-import me.sargunvohra.lib.pokekotlin.model.Pokemon
 
 
 class MainActivity : AppCompatActivity()
 {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mainactivity)
+        setTheme(R.style.AppTheme_NoActionBar);
+        val connManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+        if (mWifi.isConnected) {
+            setContentView(R.layout.mainactivity)            // Do whatever
+        }
+        else{
+            val builder = AlertDialog.Builder(this)
+
+            with(builder)
+            {
+                builder.setTitle("Error No Wifi Detected!")
+                builder.setMessage("Please Come Back Later")
+                builder.setPositiveButton("OK") { dialog: DialogInterface?, whichButton: Int ->
+                    this@MainActivity.finish();
+                    System.exit(0)
+                }
+                builder.show()
+            }
+        }
+
     }
 }
